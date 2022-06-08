31cd20eedb2f1855bee8ed500358998f7dfdd180
jellylab <jellylabgaming@gmail.com>
2022-06-08 15:33:20 +0800

    v4.0 + Banwave checker

--- src/main/java/com/jelly/farmhelper/macros/MacroHandler.java
+++ src/main/java/com/jelly/farmhelper/macros/MacroHandler.java
@@ -10,6 +10,7 @@ import com.jelly.farmhelper.features.BanwaveChecker;
 import com.jelly.farmhelper.features.Scheduler;
 import com.jelly.farmhelper.utils.*;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
@@ -17,7 +18,10 @@ import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.lwjgl.input.Mouse;
+
+import java.lang.reflect.Field;
 
 public class MacroHandler {
     private static final Minecraft mc = Minecraft.getMinecraft();
@@ -114,6 +118,15 @@ public class MacroHandler {
 
     public static void enableCurrentMacro() {
         if (!currentMacro.enabled && !startingUp) {
+            mc.inGameHasFocus = true;
+            mc.displayGuiScreen((GuiScreen) null);
+            Field f = null;
+            f = FieldUtils.getDeclaredField(mc.getClass(), "leftClickCounter",true);
+            try {
+                f.set(mc, 10000);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
             startingUp = true;
             KeyBindUtils.updateKeys(false, false, false, false, false, true, false);
             new Thread(startCurrent).start();
