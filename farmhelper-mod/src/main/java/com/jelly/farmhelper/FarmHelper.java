84c1f8e1f4afb50b87779910f468970b01293f14
Yonezu <108151150+yyonezu@users.noreply.github.com>
2022-07-03 11:00:21 -0300

    small mistake

--- farmhelper-mod/src/main/java/com/jelly/farmhelper/FarmHelper.java
+++ farmhelper-mod/src/main/java/com/jelly/farmhelper/FarmHelper.java
@@ -28,7 +28,7 @@ import java.util.jar.Manifest;
 public class FarmHelper {
     public static final String MODID = "farmhelper";
     public static final String NAME = "Farm Helper";
-    public static final String VERSION = "4.1.4";
+    public static final String VERSION = "4.2.1";
     // the actual mod version from gradle properties, should match with VERSION
     public static String MODVERSION = "-1";
     public static String BOTVERSION = "-1";
@@ -87,6 +87,6 @@ public class FarmHelper {
         Attributes attr = manifest.getMainAttributes();
         MODVERSION = attr.getValue("modversion");
         BOTVERSION = attr.getValue("botversion");
-        Display.setTitle(FarmHelper.NAME + " v" + MODVERSION + " | Bing Chilling");
+        Display.setTitle(FarmHelper.NAME + " " + MODVERSION + " | Bing Chilling");
     }
 }
