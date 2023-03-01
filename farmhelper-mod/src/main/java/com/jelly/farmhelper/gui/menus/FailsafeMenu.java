b0507ecedc462398c09ebe7978ca7ba146c6293d
May2Bee <sebastian.kukawski@gmail.com>
2023-03-01 20:02:30 +0100

    Init notification won't appear if you don't want notifications. However, it will appear after toggling the option on.

--- farmhelper-mod/src/main/java/com/jelly/farmhelper/gui/menus/FailsafeMenu.java
+++ farmhelper-mod/src/main/java/com/jelly/farmhelper/gui/menus/FailsafeMenu.java
@@ -1,10 +1,14 @@
 package com.jelly.farmhelper.gui.menus;
 
+import com.jelly.farmhelper.FarmHelper;
+import com.jelly.farmhelper.config.interfaces.FailsafeConfig;
 import com.jelly.farmhelper.features.Autosell;
 import com.jelly.farmhelper.gui.components.Button;
 import com.jelly.farmhelper.gui.components.Slider;
 import com.jelly.farmhelper.gui.components.Toggle;
 import gg.essential.elementa.components.UIContainer;
+import gg.essential.universal.UMatrixStack;
+import org.jetbrains.annotations.NotNull;
 
 public class FailsafeMenu extends UIContainer {
     public FailsafeMenu() {
@@ -16,5 +20,18 @@ public class FailsafeMenu extends UIContainer {
         new Slider("Rotation check sensitivity (deg)", 10, 1, "rotationSens").setChildOf(this);
         new Slider("Ban threshold (15 mins)", 40, 1, "banThreshold").setChildOf(this);
         new Slider("Delay before reconnect (s)", 20, 1, "reconnectDelay").setChildOf(this);
+    }
+
+    private boolean oldNotifications = FailsafeConfig.notifications;
+
+    @Override
+    public void draw(@NotNull UMatrixStack matrixStack) {
+        super.draw(matrixStack);
+        if (oldNotifications != FailsafeConfig.notifications) {
+            oldNotifications = FailsafeConfig.notifications;
+            if (FailsafeConfig.notifications) {
+                FarmHelper.registerInitNotification();
+            }
+        }
     }
 }
