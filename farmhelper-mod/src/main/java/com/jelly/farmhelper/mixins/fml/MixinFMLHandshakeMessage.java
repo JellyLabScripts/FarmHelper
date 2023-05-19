514e4065b48a8285368e7515950d5760ed7388b9
SideloadedWaffle <77439254+SideloadedWaffle@users.noreply.github.com>
2023-05-19 17:31:23 +0100

    Fixed Modhider
    Fixed modhider not allowing you to join singleplayer worlds.

--- farmhelper-mod/src/main/java/com/jelly/farmhelper/mixins/fml/MixinFMLHandshakeMessage.java
+++ farmhelper-mod/src/main/java/com/jelly/farmhelper/mixins/fml/MixinFMLHandshakeMessage.java
@@ -3,6 +3,7 @@ package com.jelly.farmhelper.mixins.fml;
 import net.minecraftforge.fml.common.ModContainer;
 import net.minecraftforge.fml.common.network.handshake.FMLHandshakeMessage;
 import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,6 +18,11 @@ public abstract class MixinFMLHandshakeMessage {
 
     @Inject(method = "<init>(Ljava/util/List;)V", at = @At("RETURN"))
     private void init(List<ModContainer> modList, CallbackInfo ci) {
-        modTags.keySet().removeIf(c -> !c.matches("FML|Forge|mcp"));
+        if (!Minecraft.getMinecraft().isIntegratedServerRunning()) {
+            modTags.keySet().removeIf(c -> !c.matches("FML|Forge|mcp"));
+        }
     }
+
 }
+
+
