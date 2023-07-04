0d98c6dd2f815593a0f0a4b171dceb0677532a12
CatalizCS <catalizcs@proton.me>
2023-07-04 18:47:16 +0700

    change version number! ready for pull request

--- farmhelper-mod/src/main/java/com/jelly/farmhelper/utils/StatusUtils.java
+++ farmhelper-mod/src/main/java/com/jelly/farmhelper/utils/StatusUtils.java
@@ -16,14 +16,17 @@ public class StatusUtils {
 
         if (!MacroHandler.isMacroing) {
             setStateString("Idling");
-        } else if (Failsafe.emergency && Failsafe.restartAfterFailsafeCooldown.isScheduled() && !Failsafe.restartAfterFailsafeCooldown.passed()) {
+        }
+        else if (Failsafe.emergency && Failsafe.restartAfterFailsafeCooldown.isScheduled() && !Failsafe.restartAfterFailsafeCooldown.passed()) {
             setStateString("Restart after " + Utils.formatTime(Failsafe.restartAfterFailsafeCooldown.getEndTime() - System.currentTimeMillis()));
-        } else if (!Failsafe.emergency && Failsafe.jacobWait.passed()) {
+        }
+        else if (!Failsafe.emergency && Failsafe.jacobWait.passed()) {
             setStateString(Scheduler.getStatusString());
         }
-        else {
+        else if (Failsafe.jacobWait.getEndTime() - System.currentTimeMillis() > 0) {
             setStateString("Jacob failsafe for " + Utils.formatTime(Failsafe.jacobWait.getEndTime() - System.currentTimeMillis()));
         }
+        else setStateString("Idling");
         cookieFail = "AutoCookie Fail: " + AutoCookie.failCount + (AutoCookie.failCount == 3 ? " (OFF)" : "");
         potFail = "AutoPot Fail: " + AutoPot.failCount + (AutoPot.failCount == 3 ? " (OFF)" : "");
     }
