1a54491e200097fc9140d06813bd748e62334a3b
NXlSChrona <70793032+NXlSChrona@users.noreply.github.com>
2023-07-19 05:30:39 -0500

    Update SShapeCropMacro.java
    ok 5th look at this, I think this would work to allow blackcat macro to just copy melon/pumpkin and only change pitch. 
    I didn't see anything anywhere where the gui actually determined the macro so i did something probably goofy looking

--- farmhelper-mod/src/main/java/com/jelly/farmhelper/macros/SShapeCropMacro.java
+++ farmhelper-mod/src/main/java/com/jelly/farmhelper/macros/SShapeCropMacro.java
@@ -81,9 +81,11 @@ public class SShapeCropMacro extends Macro {
         if (crop == CropEnum.NETHER_WART || crop == CropEnum.CACTUS) {
             pitch = (float) (0f + Math.random() * 0.5f);
         } else if (crop == CropEnum.MELON || crop == CropEnum.PUMPKIN) {
-            pitch = 28 + (float) (Math.random() * 2); //28-30
-        } else if (crop == CropEnum.BLACK_CAT || crop == CropEnum.BLACK_CAT) {
-            pitch = 58.5 + (float) (Math.random() * 0.25); //58.5-59
+            if VerticalMacroType == 8 {
+            pitch = 58.5 + (float) (Math.random() * 0.25); //58.5-59 
+            } else { 
+                pitch = 28 + (float) (Math.random() * 2); //28-30
+            }
         } else {
             pitch = (float) (2.8f + Math.random() * 0.5f);
         }
