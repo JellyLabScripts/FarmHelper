7d564b68f2ee343409561966b3ea1c9ddf5b562c
TomJuri <mail@tomjuri.de>
2023-07-22 20:54:34 +0200

    version gets set by blossom
    only change the one in build.gradle.kts file

--- farmhelper-mod/src/main/java/com/jelly/farmhelper/FarmHelper.java
+++ farmhelper-mod/src/main/java/com/jelly/farmhelper/FarmHelper.java
@@ -36,7 +36,7 @@ import java.util.jar.Manifest;
 public class FarmHelper {
     public static final String MODID = "farmhelper";
     public static final String NAME = "Farm Helper";
-    public static final String VERSION = "4.5.0-PRE";
+    public static final String VERSION = "%%VERSION%%";
 
     // the actual mod version from gradle properties, should match with VERSION
     public static String MODVERSION = "-1";
