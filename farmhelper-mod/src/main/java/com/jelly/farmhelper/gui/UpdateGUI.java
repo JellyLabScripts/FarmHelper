3d870308f9dc10f3a65f883ef150593af528804e
onixiya1337 <47077455+onixiya1337@users.noreply.github.com>
2023-07-23 18:39:48 +0200

    Added scrollable Updater GUI

--- farmhelper-mod/src/main/java/com/jelly/farmhelper/gui/UpdateGUI.java
+++ farmhelper-mod/src/main/java/com/jelly/farmhelper/gui/UpdateGUI.java
@@ -1,17 +1,31 @@
 package com.jelly.farmhelper.gui;
 
+import cc.polyfrost.oneconfig.config.core.OneColor;
 import com.jelly.farmhelper.network.APIHelper;
-import com.jelly.farmhelper.utils.Utils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraft.client.gui.GuiScreen;
+import org.apache.commons.io.FileUtils;
 import org.json.simple.JSONObject;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.GL11;
 
 import java.awt.*;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+
+import net.minecraft.client.gui.GuiSlot;
 
 import static com.jelly.farmhelper.FarmHelper.MODVERSION;
 import static com.jelly.farmhelper.FarmHelper.VERSION;
@@ -21,34 +35,83 @@ public class UpdateGUI extends GuiScreen {
     private static boolean shownGui = false;
     public static boolean outdated = false;
     private GuiButton downloadBtn;
-    private static String[] message;
     private GuiButton closeBtn;
+    static String latestVersion = "";
+    private int displayGUI = 0;
+    private int downloadProgress = 0;
+    protected OneColor color = new OneColor(0, 0, 0, 255);
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+    private static int GUI_WIDTH = mc.displayWidth;
+    private static final int GUI_HEIGHT = mc.displayHeight / mc.gameSettings.guiScale - 160;
+    private static final int LIST_TOP_MARGIN = 160 / mc.gameSettings.guiScale;
+    private static final int LIST_BOTTOM_MARGIN = 80 / mc.gameSettings.guiScale;
+    private static List<String> message = new ArrayList<>();
+    private ScrollableList scrollableList;
 
     @Override
-    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) { // don't skid or i'll find you - yuro
         this.drawBackground(0);
-        float scale = 3;
+        scrollableList.drawScreen(mouseX, mouseY, partialTicks);
+        float scale = mc.gameSettings.guiScale;
         GL11.glScalef(scale, scale, 0.0F);
-        this.drawCenteredString(mc.fontRendererObj, "Outdated version of FarmHelper", (int) (this.width / 2f / scale), (int) (this.height / 6f / scale), Color.RED.darker().getRGB());
-        GL11.glScalef(1.0F / scale, 1.0F / scale, 0.0F);
-        scale = 1.5f;
-        GL11.glScalef(scale, scale, 0.0F);
-        this.drawString(mc.fontRendererObj, "What's new? ➤", (int) (this.width / 2f / scale - 180), (int) (this.height / 6 / scale + 25), Color.GREEN.getRGB());
-        GL11.glScalef(1.0F / scale, 1.0F / scale, 0.0F);
-        if (message != null) {
-            int y = 40;
-            for (String s : message) {
-                this.drawString(mc.fontRendererObj, s, this.width / 2 - 160, this.height / 6 + y, Color.WHITE.getRGB());
-                y += 15;
-            }
+        if (displayGUI == 0)
+            this.drawCenteredString(mc.fontRendererObj, "Outdated version of FarmHelper", (int) (this.width / 2f / scale), (int) (30 / scale), Color.RED.darker().getRGB());
+        if (displayGUI == 1) {
+            Color chroma = Color.getHSBColor((float) ((System.currentTimeMillis() / 10) % 500) / 500, 1, 1);
+            color.setFromOneColor(new OneColor(chroma.getRed(), chroma.getGreen(), chroma.getBlue(), 255));
+            this.drawCenteredString(mc.fontRendererObj, "Updating FarmHelper... (" + downloadProgress + "%) Please wait", (int) (this.width / 2f / scale), (int) (30 / scale), OneColor.HSBAtoARGB(color.getHue(), color.getSaturation(), color.getBrightness(), color.getAlpha()));
         }
+        if (displayGUI == 2)
+            this.drawCenteredString(mc.fontRendererObj, "FarmHelper has been updated!", (int) (this.width / 2f / scale), (int) (30 / scale), Color.GREEN.getRGB());
+        GL11.glScalef(1.0F / scale, 1.0F / scale, 0.0F);
+        scale = 1f;
+        GL11.glScalef(scale, scale, 0.0F);
+        this.drawString(mc.fontRendererObj, "What's new? ➤", (int) (this.width / 8f / scale), GUI_HEIGHT / mc.gameSettings.guiScale + 25, Color.GREEN.getRGB());
+        GL11.glScalef(1.0F / scale, 1.0F / scale, 0.0F);
         super.drawScreen(mouseX, mouseY, partialTicks);
     }
 
     @Override
     public void initGui() {
         super.initGui();
+        GUI_WIDTH = mc.displayWidth / mc.gameSettings.guiScale; // fix for changing window size
+        scrollableList = new ScrollableList(GUI_WIDTH, GUI_HEIGHT, LIST_TOP_MARGIN, height - LIST_BOTTOM_MARGIN, 10);
+        scrollableList.registerScrollButtons(7, 8);
         registerButtons();
+    }
+
+    private class ScrollableList extends GuiSlot {
+        private ScrollableList(int width, int height, int top, int bottom, int slotHeight) {
+            super(UpdateGUI.mc, width, height, top, bottom, slotHeight);
+        }
+
+        @Override
+        protected int getSize() {
+            return message.size();
+        }
+
+        @Override
+        protected void elementClicked(int slotIndex, boolean isDoubleClick, int mouseX, int mouseY) {}
+
+        @Override
+        protected boolean isSelected(int index) { return false; }
+
+        @Override
+        protected void drawBackground() {}
+
+        @Override
+        protected void drawSlot(int entryID, int insideLeft, int yPos, int insideSlotHeight, int mouseXIn, int mouseYIn) {
+            if (entryID >= 0 && entryID < message.size()) {
+                String item = message.get(entryID);
+                drawString(mc.fontRendererObj, item, (int) (mc.displayWidth / 8f / mc.gameSettings.guiScale), yPos + 2, Color.WHITE.getRGB());
+            }
+        }
+
+        @Override
+        protected int getContentHeight() { return getSize() * slotHeight; }
+
+        @Override
+        protected int getScrollBarX() { return GUI_WIDTH - 12 / mc.gameSettings.guiScale; }
     }
 
     @Override
@@ -61,25 +124,93 @@ public class UpdateGUI extends GuiScreen {
     }
 
     @Override
-    protected void actionPerformed(GuiButton button)  {
-        switch (button.id) {
-            case 1: // closebtn
-                mc.displayGuiScreen(new GuiMainMenu());
-                break;
-            case 2: // downloadbtn
-                Utils.openURL("https://github.com/Jelly298/FarmHelper/releases/latest");
-                mc.displayGuiScreen(new GuiMainMenu());
+    public void updateScreen() { super.updateScreen(); }
 
-                break;
+    @Override
+    protected void actionPerformed(GuiButton button)  {
+        if (button.enabled) {
+            switch (button.id) {
+                case 1: // downloadBtn
+                    if (displayGUI != 0) return;
+                    if (latestVersion == null || latestVersion.isEmpty()) return;
+                    executor.submit(() -> {
+                        int CONNECT_TIMEOUT = 10000;
+                        int READ_TIMEOUT = 10000;
+                        try {
+                            downloadBtn.enabled = false;
+                            closeBtn.enabled = false;
+                            displayGUI = 1;
+                            //FileUtils.copyURLToFile(new URL("https://github.com/JellyLabScripts/FarmHelper/releases/download/" + latestVersion + "/FarmHelper-" + latestVersion + ".jar"), new File(mc.mcDataDir + "/mods/farmhelper-mod-" + latestVersion + ".jar"), CONNECT_TIMEOUT, READ_TIMEOUT);
+                            downloadFileWithProgress(("https://github.com/JellyLabScripts/FarmHelper/releases/download/" + latestVersion + "/FarmHelper-" + latestVersion + ".jar"), new File(mc.mcDataDir + "/mods/farmhelper-mod-" + latestVersion + ".jar"), CONNECT_TIMEOUT, READ_TIMEOUT);
+                            mc.addScheduledTask(() -> {
+                                downloadBtn.enabled = false;
+                                closeBtn.enabled = true;
+                                displayGUI = 2;
+                            });
+                        } catch (IOException e) {
+                            downloadBtn.enabled = true;
+                            closeBtn.enabled = true;
+                            displayGUI = 0;
+                            e.printStackTrace();
+                        }
+                    });
+                    break;
+                case 2: // closeBtn
+                    if (displayGUI == 0) mc.displayGuiScreen(new GuiMainMenu());
+                    if (displayGUI == 2) {
+                        mc.addScheduledTask(() -> {
+                            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                                File fileToDelete = new File(mc.mcDataDir + "/mods/farmhelper-mod-v" + VERSION + ".jar");
+                                if (fileToDelete.exists()) {
+                                    try {
+                                        FileUtils.forceDelete(fileToDelete);
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }));
+                        });
+                        mc.shutdown();
+                    }
+                    break;
+            }
+        }
+        scrollableList.actionPerformed(button);
+    }
+
+    @Override
+    public void handleMouseInput() throws IOException {
+        super.handleMouseInput();
+        scrollableList.handleMouseInput();
+    }
+
+    private void downloadFileWithProgress(String downloadURL, File outputFile, int connectTimeout, int readTimeout) throws IOException {
+        URL url = new URL(downloadURL);
+        URLConnection connection = url.openConnection();
+        connection.setConnectTimeout(connectTimeout);
+        connection.setReadTimeout(readTimeout);
+
+        int fileSize = connection.getContentLength();
+        int downloadedBytes = 0;
+
+        try (InputStream in = connection.getInputStream();
+             FileOutputStream out = new FileOutputStream(outputFile)) {
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+                downloadedBytes += bytesRead;
+                downloadProgress = (int) ((downloadedBytes / (double) fileSize) * 100);
+            }
         }
     }
 
     private void registerButtons() {
-        this.closeBtn = new GuiButton(1, this.width / 2, this.height / 2 + 100, 150, 20, "Close");
-        this.buttonList.add(this.closeBtn);
-
-        this.downloadBtn = new GuiButton(2, this.width / 2 - 150, this.height / 2 + 100, 150, 20, "Download new version");
+        this.downloadBtn = new GuiButton(1, this.width / 2 - 152, this.height - 30, 150, 20, "Download new version");
         this.buttonList.add(this.downloadBtn);
+        this.closeBtn = new GuiButton(2, this.width / 2 + 2, this.height - 30, 150, 20, "Close");
+        this.buttonList.add(this.closeBtn);
     }
 
     public static void showGUI() {
@@ -87,16 +218,16 @@ public class UpdateGUI extends GuiScreen {
             mc.displayGuiScreen(new UpdateGUI());
             shownGui = true;
             outdated = isOutdated();
-            message = getReleaseMessage().replaceAll("\r", "").replace("+ ", "§a+ ").replace("= ", "§f= ").replace("- ", "§c- ").split("\n");
+            message = Arrays.asList(getReleaseMessage().replaceAll("\r", "").replace("+ ", "§a+ ").replace("= ", "§f= ").replace("- ", "§c- ").split("\n"));
         }
     }
     private static boolean isOutdated() {
         try {
-            String latestversion = APIHelper.readJsonFromUrl("https://api.github.com/repos/Jelly298/FarmHelper/releases/latest",
+            latestVersion = APIHelper.readJsonFromUrl("https://api.github.com/repos/JellyLabScripts/FarmHelper/releases/latest",
                             "User-Agent",
                             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36")
                     .get("tag_name").toString();
-            return (!MODVERSION.toLowerCase().contains(latestversion.toLowerCase()) && !VERSION.toLowerCase().contains("pre"));
+            return (!MODVERSION.toLowerCase().contains(latestVersion.toLowerCase()) && !VERSION.toLowerCase().contains("pre"));
         } catch (Exception e) {
             return false;
         }
@@ -104,13 +235,12 @@ public class UpdateGUI extends GuiScreen {
 
     private static String getReleaseMessage() {
         try {
-            JSONObject j = APIHelper.readJsonFromUrl("https://api.github.com/repos/Jelly298/FarmHelper/releases/latest",
+            JSONObject j = APIHelper.readJsonFromUrl("https://api.github.com/repos/JellyLabScripts/FarmHelper/releases/latest",
                             "User-Agent",
                             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36");
-            return j.get("body").toString();
+            return "\n" + j.get("body").toString() + "\n\n"; // fix for top and bottom padding
         } catch (Exception e) {
             return "No release message was found.";
         }
     }
-
 }
