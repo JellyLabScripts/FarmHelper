9aa4f3635c9dec6bb5f66db1db9d38e9108ea311
HonxY <54810511+HonxY@users.noreply.github.com>
2023-08-04 13:58:36 +0300

    Update SShapeCropMacro.java

--- farmhelper-mod/src/main/java/com/jelly/farmhelper/macros/SShapeCropMacro.java
+++ farmhelper-mod/src/main/java/com/jelly/farmhelper/macros/SShapeCropMacro.java
@@ -84,7 +84,7 @@ public class SShapeCropMacro extends Macro {
             if (crop == CropEnum.NETHER_WART || crop == CropEnum.CACTUS) {
                 pitch = (float) (0f + Math.random() * 0.5f);
             } else if (crop == CropEnum.MELON || crop == CropEnum.PUMPKIN) {
-                pitch = -59
+                pitch = -59;
             } else {
                 pitch = (float) (2.8f + Math.random() * 0.5f);
             }
