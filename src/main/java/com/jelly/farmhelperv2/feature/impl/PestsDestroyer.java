924b451d9235a8f9ba6b865d46f19d490e461077
xai <xai@xaine.dev>
2023-11-26 18:55:12 +0000

    use vacuum to find pest location if cant find plot number

--- src/main/java/com/jelly/farmhelperv2/feature/impl/PestsDestroyer.java
+++ src/main/java/com/jelly/farmhelperv2/feature/impl/PestsDestroyer.java
@@ -1008,6 +1008,20 @@ public class PestsDestroyer implements IFeature {
                 } else if (StringUtils.stripControlCodes(slot.getStack().getDisplayName()).equals("The Barn"))
                     plotCounter++;
             }
+
+            if (pestsPlotMap.isEmpty()) {
+                long delay = 500 + (long) (Math.random() * 500);
+                delayClock.schedule(delay + (long)(Math.random() * 200));
+                LogUtils.sendError("[Pests Destroyer] Failed to get locations of pests. (Uncleaned plots?)");
+                LogUtils.sendError("[Pests Destroyer] Attempting to find pest with tracker");
+                state = States.GET_LOCATION;
+                Multithreading.schedule(() -> {
+                    if (mc.currentScreen != null) {
+                        mc.thePlayer.closeScreen();
+                    }
+                }, delay, TimeUnit.MILLISECONDS);
+                return;
+            }
         }
         int pestsInMap = pestsPlotMap.values().stream().mapToInt(i -> i).sum();
         if (pestsInMap < amountOfPests) return;
