f048a13aa561ea198048dab671f7528c2d14d3ea
xai <xai@xaine.dev>
2024-01-08 19:28:43 +0000

    update delay

--- src/main/java/com/jelly/farmhelperv2/feature/impl/AutoSprayonator.java
+++ src/main/java/com/jelly/farmhelperv2/feature/impl/AutoSprayonator.java
@@ -48,7 +48,7 @@ public class AutoSprayonator implements IFeature {
     private final Clock sprayonatorDelay = new Clock();
     @Getter
     private final Clock enableDelay = new Clock();
-    private final long sprayonatorEnableDelayTime = 1200L;
+    private final long sprayonatorEnableDelayTime = 5200L;
     private long sprayonatorEnableDelayTimeRand = 0L;
     {
         updateRandomDelay();
@@ -193,7 +193,7 @@ public class AutoSprayonator implements IFeature {
     }
 
     private void updateRandomDelay() {
-        sprayonatorEnableDelayTimeRand = (long) (Math.random() * 225);
+        sprayonatorEnableDelayTimeRand = (long) (Math.random() * 765);
     }
 
     @SubscribeEvent
